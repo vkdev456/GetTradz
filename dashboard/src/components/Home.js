9f24d5ab32d7f60d7de04430dbc1a5c7000d582e
kviswas1216 <kviswas1216@gmail.com>
2025-07-26 15:36:14 +0530

    Dashboard

---
+++ dashboard/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Dashboard from "./Dashboard";
+import TopBar from "./Topbar";
+
+const Home = () => {
+  return (
+    <>
+      <TopBar />
+      <Dashboard />
+    </>
+  );
+};
+
+export default Home;
