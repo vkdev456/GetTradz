40d06a5b78b3b7ded81726f2ab233f576ba2d1a8
kviswas1216 <kviswas1216@gmail.com>
2025-08-11 16:02:04 +0530

    Fetching Dashboard data from cloud-Atlas

--- dashboard/src/components/Holdings.js
+++ dashboard/src/components/Holdings.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios, { all } from "axios";
+import axios from "axios";
 import { VerticalGraph } from "./VerticalGraph";
 
 
@@ -9,10 +9,10 @@ const Holdings = () => {
 
   useEffect(() => {
     axios.get("http://localhost:3002/allHoldings").then((res) => {
-      // console.log(res.data);
       setAllHoldings(res.data);
     });
   }, []);
+
 
  
   const labels = allHoldings.map((subArray) => subArray["name"]);
